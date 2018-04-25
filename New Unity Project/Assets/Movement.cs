532b4c97c7b4714d5537dc826fdfe0fc0182b1ee
Unknown <kami.zeron@gmail.com>
2018-04-25 17:54:26 -0300

    a

--- New Unity Project/Assets/Movement.cs
+++ New Unity Project/Assets/Movement.cs
@@ -6,16 +6,18 @@ public class Movement : MonoBehaviour {
 
     [SerializeField] float MovementSpeed = 10;
     [SerializeField] float RotationSpeed = 10;
+    Time t;
 
 	// Use this for initialization
 	void Start () {
-		
+     
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
-        transform.position += new Vector3(MovementSpeed * horizontal, 0, MovementSpeed * vertical);
+        transform.position += new Vector3(MovementSpeed * horizontal * Time.deltaTime, 0, MovementSpeed * vertical * Time.deltaTime);
 	}
 }
